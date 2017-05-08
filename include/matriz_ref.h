07da4d9e111cf0b3baf403fb5a523c00e1efce79
Turambar <miguel.lorenzo.1990@gmail.com>
2017-05-08 15:04:01 -0500

    asfasf

--- include/matriz_ref.h
+++ include/matriz_ref.h
@@ -1,11 +1,11 @@
 #ifndef MATRIZ_REF_H
 #define MATRIZ_REF_H
 
-
+template<typename T, size_t N>
 class matriz_ref
 {
   public:
-    matriz_ref();
+    matriz_ref(const matriz);
     virtual ~matriz_ref();
 
   protected:
